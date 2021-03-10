3bd059ad5cd9462bb7c75767cfd555b6850015bb
Tina Haiser <tinahaiser@gmail.com>
2021-03-10 01:45:55 +0000

    Initial commit because we were struggling with how to include the third person
    Co-authored-by: Deborah Ong <Deborah.oyt@gmail.com>
    Co-authored-by: Karen Reynolds <k.reynolds102@gmail.com>

---
+++ madlibs.py
@@ -0,0 +1,67 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
+    'smashing', 'lovely',
+]
+
+@app.route('/game')
+def show_madlib_form():
+    """Go to the game form"""
+    
+    answer = request.args.get("answer")
+    if answer == "Yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route('/madlib', methods = ["POST"])
+def show_mad_lib():
+    """Create the madlib"""
+
+    color = request.form.get('colour')
+    noun = request.form.get('noun')
+    adjective = request.form.get('adjective')
+
+
+@app.route('/')
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+
+if __name__ == '__main__':
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0")
